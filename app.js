03cea595b9c17d61b32768f01196468a09b203b3
Ankit Saini <ankitsaini8999@gmail.com>
2021-08-27 18:40:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const section = document.getElementById("abc");
+const box = document.getElementById("box");
+const eye1 = document.querySelector(".eye1");
+const eye2 = document.querySelector(".eye2");
+const mouth = document.querySelector(".mouth");
+const butt = document.getElementById("butt");
+
+section.addEventListener("mousemove", (e) => {
+	let x = e.pageX;
+	let y = e.pageY;
+	let posX = x / innerWidth;
+	let posY = y / innerHeight;
+	eye1.style.transform = `translate(${posX * 70}px,${posY * 70}px)`;
+	eye2.style.transform = `translate(${posX * 70}px,${posY * 70}px)`;
+});
+
+butt.addEventListener("mouseenter", () => {
+	mouth.classList.add("active");
+});
+butt.addEventListener("mouseleave", () => {
+	mouth.classList.remove("active");
+});
